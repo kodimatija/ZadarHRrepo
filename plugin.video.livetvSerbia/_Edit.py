6255377588b4452bc1c96357a01f217e71c3ed5d
Matija <matijaivanic.51477@gmail.com>
2019-10-21 17:17:38 +0200

    v-1.3.7.5

--- plugin.video.livetvSerbia/_Edit.py
+++
@@ -1,5 +0,0 @@
-import xbmcaddon
-
-MainBase = ('WVVoU01HTklUVFpNZVRsellWaGFiR1JJV25KaU1sSndZekpXZVZsdGJHaE1iVTUyWWxN'.decode('base64')+'NWFGcEhVblppYm1oMFlrTTVjbGxYTldoaVIydDJZVEk1YTJGWVRubFpiV3h4V1ZNMWQyRklRVDA9'.decode('base64')).decode('base64').decode('base64')
-LSProFile = 'https://livetvkodiserbia.com/addonxml/kanali/LSProdynamicCode.py'
-addon = xbmcaddon.Addon('plugin.video.livetvSerbia')
